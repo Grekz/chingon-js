c06a229940d65d6037d482cb472b1535f310643f
Adán Carrasco <adancarrasco@users.noreply.github.com>
2019-04-21 00:32:21 +0200

    Add rollup (#3)
    * Add rollup
    
    * Add temporary fix for rollup-plugin-generate-html-template import issue
    
    * Move code after 'use strict'
    
    * Use shrinkwrap

---
+++ src/htmlTemplate.js
@@ -0,0 +1,72 @@
+"use strict";
+
+console.warn("FIX ME!");
+
+import { readFile, writeFile } from "fs";
+import path from "path";
+
+/**
+ * Takes an HTML file as a template then adds the bundle to the final file.
+ * @param {Object} options The options object.
+ * @return {Object} The rollup code object.
+ */
+export default function htmlTemplate(options = {}) {
+  const { template, target } = options;
+
+  // Get the target file name.
+  const targetName = path.basename(target || template);
+  // Add the file suffix if it isn't there.
+  const targetFile =
+    targetName.indexOf(".html") < 0 ? `${targetName}.html` : targetName;
+
+  return {
+    name: "html-template",
+    generateBundle: function generateBundle(outputOptions) {
+      const bundle = outputOptions.file;
+      return new Promise((resolve, reject) =>
+        readFile(template, (err, buffer) => {
+          if (err) {
+            return reject(err);
+          }
+
+          // Convert buffer to a string and get the </body> index.
+          const tmpl = buffer.toString("utf8");
+          const bodyCloseTag = tmpl.lastIndexOf("</body>");
+
+          // Inject the script tag before the body close tag.
+          const injected = [
+            tmpl.slice(0, bodyCloseTag),
+            `<script src="${path.basename(bundle)}"></script>\n`,
+            tmpl.slice(bodyCloseTag, tmpl.length)
+          ].join("");
+
+          // Write the injected template to a file.
+          promisify(
+            writeFile,
+            path.join(path.dirname(bundle), targetFile),
+            injected
+          ).then(() => resolve(), e => reject(e));
+        })
+      );
+    }
+  };
+}
+
+/**
+ * Promisify's a function.
+ * @param {Function} fn The function to turn into a promise.
+ * @param {any[]} args The arguments for the function.
+ * @return {Promise} A Promise for the function.
+ */
+function promisify(fn, ...args) {
+  return new Promise((resolve, reject) => {
+    try {
+      fn(...args, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
