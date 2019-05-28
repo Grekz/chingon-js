81a936d0d5ce6a482944a2a0a8516af173ab34ee
Grekz <vidbla.grekz@gmail.com>
2019-05-28 21:46:46 +0200

    Update dependencies + storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,37 +1,7 @@
-import { configure, setAddon, addDecorator } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
 import { checkA11y } from '@storybook/addon-a11y'
-import { withInfo } from '@storybook/addon-info'
 
-// Globally in your .storybook/config.js, or alternatively, per-chapter
-addDecorator(
-  withInfo({
-    styles: {
-      header: {
-        h1: {
-          marginRight: '20px',
-          fontSize: '25px',
-          display: 'inline',
-        },
-        body: {
-          paddingTop: 0,
-          paddingBottom: 0,
-        },
-        h2: {
-          display: 'inline',
-          color: '#999',
-        },
-      },
-      infoBody: {
-        backgroundColor: '#eee',
-        padding: '0px 5px',
-        lineHeight: '2',
-      },
-    },
-    inline: true,
-    source: false,
-  })
-)
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.tsx$/)
 
