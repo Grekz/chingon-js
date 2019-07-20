bae97ae16605c0d4534a0a8327f90292cd2ef3b1
Adán Carrasco <adan.carrasco@hotmail.com>
2019-07-20 18:13:28 +0200

    Create Button component

--- src/atoms/Button/button.test.tsx
+++ src/atoms/Button/button.test.tsx
@@ -4,22 +4,25 @@ import toJson from 'enzyme-to-json'
 import serializer, { matchers } from 'jest-emotion'
 import * as React from 'react'
 import Button from '.'
+import { ButtonTypes } from './button.component'
 
 configure({ adapter: new Adapter() })
 expect.extend(matchers)
 expect.addSnapshotSerializer(serializer)
 
+const { PRIMARY } = ButtonTypes
+
 describe('<Button /> rendering', () => {
   it('should render Button', () => {
-    const wrapper = shallow(<Button />)
+    const wrapper = shallow(<Button buttonType={PRIMARY} />)
     expect(wrapper).toHaveLength(1)
   })
   it('hidden modal matches the snapshot', () => {
-    const tree = shallow(<Button />)
+    const tree = shallow(<Button buttonType={PRIMARY} />)
     expect(toJson(tree)).toMatchSnapshot()
   })
   it('shown modal matches the snapshot', () => {
-    const tree = shallow(<Button color={'white'} />)
+    const tree = shallow(<Button buttonType={PRIMARY} />)
     expect(toJson(tree)).toMatchSnapshot()
   })
 })
